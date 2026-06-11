96f4e19963c699c79f98b69a0489243507c52402
Praptea-code <bhattarai.prapti00@gmail.com>
2026-06-11 09:53:49 +0545

    feat: add document files and wrote code to show first two documents

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -24,15 +24,24 @@ def load_documents(docs_path="docs"):
     #load all .txt files from the directory docs
     loader= DirectoryLoader(
         path=docs_path,
-        glob="*.txt" #only look for txt files
+        glob="*.txt", #only look for txt files
         loader_cls=TextLoader #using txt loader class we imported this before 
-    )
+        )
 
     documents=loader.load() # this will list langchain documents which have page content, attributes and metadata arttibutes
 
     if len(documents)==0:
         raise FileNotFoundError("No .txt files found add your documents first")
     
+    for i, doc in enumerate(documents[:2]):
+        print(f"\nDocument {i+1}:")
+        print(f" Source:{doc.metadata['source']}")
+        print(f"Content length: {len(doc.page_content)} characters")
+        print(f"Content preview: {doc.page_content[:100]}")
+        print(f"metadata:{doc.metadata}")
+    
+    return documents 
+
 def main():
     print("Main function")
 
