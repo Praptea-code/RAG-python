3c9e8f3f71148c5bef50044057c21f330b9b348c
Praptea-code <bhattarai.prapti00@gmail.com>
2026-06-13 00:17:21 +0545

    feat: load our llm model llama3.2 and made the prompt message , send the reauest to lllm and print it

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -27,6 +27,31 @@ query="What was the original name of Microsoft before it became Microsoft?"
 relevant_docs=retriever.invoke(query)
 
 print(f"User Query :{query}")
+
 print("Context")
 for i, doc in enumerate(relevant_docs,1):
     print(f"Document {i}: \n {doc.page_content} \n")
+
+#this is the prompt prepared to send to the llm model
+#we pass the query and take every document and extract its text
+combined_input= f"""Based on the following documents, please answer this question :{query} 
+
+Documents:
+{chr(10).join([f"{doc.page_content}" for doc in relevant_docs])}
+Please provide clear and helpful answer but only use the information from these documents. IF you cannot dins the answer then say "I dont have enought information to answer the questions"
+"""
+#this is our llm model we load the llm model
+model= ChatOllama(model="llama3.2")
+
+messages = [
+    SystemMessage(content="You are a helpful assistant"), #the system is setting the behavior, rules as modern chat models do not take  single plain prompt anymore
+    HumanMessage(content=combined_input), #the actual message that llm should follow
+]
+
+#sending request to llm
+result =model.invoke(messages)
+
+print("Generated Response")
+print("Content only:")
+#ai message sends content we are just taking the text from it 
+print(result.content)
